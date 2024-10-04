957724773ff603f36b32e0368cbe37fa826d67b9
Abdul Khan <abdulrahman.h62@gmail.com>
2024-10-04 11:29:40 -0500

    updated files

---
+++ src/test/java/main/java/com/example/MainTest.java
@@ -0,0 +1,9 @@
+package main.java.com.example;
+
+public class MainTest {
+
+    public void testMain() {
+        Main mainInstance = new Main(); // Ensure Main is accessible
+        // Your test logic here
+    }
+}
