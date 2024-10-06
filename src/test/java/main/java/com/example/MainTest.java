7425dd1e3dbbf89a2d03a73b2e1a23a4b2150377
Abdul Khan <abdulrahman.h62@gmail.com>
2024-10-06 10:26:04 -0500

    updated docker and jenkins file

---
+++ src/test/java/main/java/com/example/MainTest.java
@@ -0,0 +1,11 @@
+package main.java.com.example;
+
+import main.java.com.example.Main;
+
+public class MainTest {
+
+    public void testMain() {
+        Main mainInstance = new Main(); // Ensure Main is accessible
+        // Your test logic here
+    }
+}
